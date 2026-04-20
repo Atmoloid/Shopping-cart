fc66a93ae85476f2270d7c9bdfb9980b61c1e771
Atmoloid <lorenzopica31@gmail.com>
2026-04-20 17:34:48 +0200

    Add dependencies and create initial Cart and Shop components
    - Updated package.json and package-lock.json to include new dependencies: autoprefixer, postcss, tailwindcss, and react
      /*border-inline: 1px solid var(--border);*/-router-dom.
    - Created Cart component with basic structure and CSS file.
    - Created Shop component with basic structure and CSS file.
    - Updated App component to reflect changes and added comments for future development.

---
+++ src/cart.jsx
@@ -0,0 +1,14 @@
+import './cart.css'
+
+function Cart() {
+    return (
+        <>
+        {/* quando un item viene aggiunto deve comparire il numero di item nel cart in real time  sia per aggiungrli che per rimuoverli */}
+
+         {/*  nel cart si devono vedere gli item e la loro quantità e rimuoverli o aumentare*/}
+
+        </>
+    )
+}
+
+export default Cart
---
+++ src/shop.jsx
@@ -0,0 +1,17 @@
+import './shop.css'
+
+function Shop() {
+    return (
+        <>
+   {/* ogni prodotto deve avere un elemento di cart, per aggiungerlo al cart, */} 
+
+   {/*inoltre aggiungere uno spazio in cui gli utenti possono type il numero di item che vogliono,aggiungere anche  due frecce in alto e in basso*/ }
+
+   {/* gli item vanno presi da Fakestore API */ }
+
+
+        </>
+    )
+}
+
+export default Shop
