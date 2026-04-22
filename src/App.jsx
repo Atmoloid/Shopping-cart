6e640dc9053f49ca89b1f8f3e50c8d7988d0661e
Atmoloid <lorenzopica31@gmail.com>
2026-04-22 17:58:40 +0200

    Add Tailwind CSS integration and update styles
    - Added Tailwind CSS as a dependency in package.json and package-lock.json.
    - Created postcss.config.js for Tailwind CSS configuration.
    - Updated App.css to include Tailwind directives and adjusted styles.
    - Introduced DatalinesWithGrid component for dynamic visual effects.
    - Updated index.css for consistent styling with Tailwind.
    - Created tailwind.config.js for Tailwind CSS configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 /*import {useState} from 'react' */
-/*import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'*/
 import './App.css'
+import { DatalinesWithGrid } from "/home/lorenzo/Shopping-cart/src/components/neonblade-ui/datalines-with-grid/index.tsx";
 
 function App() {
   /*const [count, setCount] = useState(0)*/
 
   return (
     <>
-      {/* Qui ci deve stare il titolo e tutto ciò che lo accompagna compresa la nav bar carta bianca su cosa aggiungere*/}
+    {/* Qui ci deve stare il titolo e tutto ciò che lo accompagna compresa la nav bar carta bianca su cosa aggiungere*/}
+    <div className="relative w-screen h-screen">
+      <DatalinesWithGrid />
+      </div>
+        
+        {/* qui ci deve stare il componente del cart, usare react router per accedere al cart */}
 
-      {/* qui ci deve stare il componente del cart, usare react router per accedere al cart */}
-
-      {/* qui ci deve stare gli item da mettere nel cart */}
+        {/* qui ci deve stare gli item da mettere nel cart */}
+      
     </>
   )
 }
